4ac65adc4b4744a6575d9739c54e34f3068437c3
Nikos Barkas <another_cmdr@oolite.org>
2012-12-31 09:00:28 +0000

    Here goes another year. Happy 2013. The Reference Sheet and the Readme still need to have year references bumped.
    git-svn-id: http://svn.berlios.de/svnroot/repos/oolite-linux/trunk/tools/planettool@5596 127b21dd-08f5-0310-b4b7-95ae10353056

--- FloatPixMap/FloatPixMap.c
+++ FloatPixMap/FloatPixMap.c
@@ -2,7 +2,7 @@
 	FloatPixMap.c
 	
 	
- 	Copyright © 2009–2012 Jens Ayton
+ 	Copyright © 2009–2013 Jens Ayton
  
 	Permission is hereby granted, free of charge, to any person obtaining a
 	copy of this software and associated documentation files (the “Software”),
